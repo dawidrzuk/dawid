89602e27bd1890b2f308d3dc63ce409a528bdd67
dawidrzuk <dawidrzuk@gmail.com>
2022-01-12 10:52:47 +0100

    dodane zdj + dziala skala

--- app/src/main/java/com/example/dawid/MainActivity.kt
+++ app/src/main/java/com/example/dawid/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.dawid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pies1)
+        findViewById<Button>(R.id.button3).setOnClickListener {
+            findViewById<ImageView>(R.id.imageView).setScaleX(1.5f)
+        }
     }
 }
