2a0a8dd0a8d2a3756091090ad9b4e644cb073f44
dawidrzuk <dawidrzukk@gmail.com>
2022-01-18 20:33:07 +0100

    dzialajaca zmiana + skala X

--- app/src/main/java/com/example/dawid/MainActivity.kt
+++ app/src/main/java/com/example/dawid/MainActivity.kt
@@ -10,16 +10,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pies1)
         findViewById<Button>(R.id.button3).setOnClickListener {
             findViewById<ImageView>(R.id.imageView).setScaleX(1.5f)
-        findViewById<Button>(R.id.button).setOnClickListener {
-            findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pies1)
         }
-            findViewById<Button>(R.id.button2).setOnClickListener {
+        var x = 0;
+        findViewById<Button>(R.id.button).setOnClickListener {
+            x--
+            if (x == 1) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pies1)
+            } else if (x == 2) {
                 findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pies2)
+            } else if (x == 3) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pies3)
+            } else if (x == 4) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pies4)
+            } else if (x == 0) {
+                x = 1
             }
         }
-
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            x++
+            if (x == 1) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pies1)
+            } else if (x == 2) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pies2)
+            } else if (x == 3) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pies3)
+            } else if (x == 4) {
+                findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.pies4)
+            } else if (x == 5)
+                x = 4
+        }
     }
 }
